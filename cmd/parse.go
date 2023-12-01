e6cc657b870d28ad0fc2d530d1f901752465a489
s0ders <39492740+s0ders@users.noreply.github.com>
2023-12-01 15:44:17 +0100

    feat(cmd/parse): new parse command to return JSON infos

--- cmd/parse.go
+++ cmd/parse.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/s0ders/go-torrent-organizer/internal/parser"
 	"github.com/spf13/cobra"
 )
@@ -13,7 +15,14 @@ var parseCmd = &cobra.Command{
 	Use:   "parser",
 	Short: "Parses a torrent file name to extract informations",
 	Args:  cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return parser.Parse(args[0])
+	RunE: func(cmd *cobra.Command, args []string) (error) {
+		infos, err := parser.ParseToJSON(args[0])
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(infos)
+
+		return nil
 	},
 }
